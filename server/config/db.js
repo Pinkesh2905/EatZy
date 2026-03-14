331db58fdc649303aa176827ecae0208c8f09a45
pinkesh029 <pinkeshh2905@gmail.com>
2026-03-14 22:39:59 +0530

    Update db connection string to MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/eatzy');
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://pinkesh29:aY4ez3uxJzfmaexV@eatzy.zsydsjx.mongodb.net/?appName=EatZy');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
